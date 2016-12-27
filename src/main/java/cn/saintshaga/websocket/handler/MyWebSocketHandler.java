f97950169a0b972abd768063383f828c68055522
huang_f <huang_f@worksap.co.jp>
2016-12-27 21:51:49 +0800

    add dependency of websocket

---
+++ src/main/java/cn/saintshaga/websocket/handler/MyWebSocketHandler.java
@@ -0,0 +1,7 @@
+package cn.saintshaga.websocket.handler;
+
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyWebSocketHandler extends TextWebSocketHandler {
+	
+}
