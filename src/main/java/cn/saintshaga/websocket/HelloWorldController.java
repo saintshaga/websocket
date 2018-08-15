42bc366a266e6a474482347386848edae71a8c62
huang_f <huang_f@worksap.co.jp>
2018-08-15 17:29:16 +0800

    add more about websocket

--- src/main/java/cn/saintshaga/websocket/HelloWorldController.java
+++ src/main/java/cn/saintshaga/websocket/HelloWorldController.java
@@ -1,13 +1,30 @@
 package cn.saintshaga.websocket;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
+
 @RestController
+@Slf4j
 public class HelloWorldController {
+
+
+	@Autowired
+	private WebsocketConnectionManager connectionManager;
 
 	@RequestMapping("/")
 	public String home() {
 		return "Hello, World!";
+	}
+
+	@RequestMapping("/test1")
+	public String test1(@PathParam("id") String id) {
+		connectionManager.closeSession(id);
+
+		log.info("Session is Open:{}", connectionManager.getSession(id) != null && connectionManager.getSession(id).isOpen());
+		return "this is test1";
 	}
 }
--- src/main/java/cn/saintshaga/websocket/handler/MyWebSocketHandler.java
+++ src/main/java/cn/saintshaga/websocket/handler/MyWebSocketHandler.java
@@ -1,14 +1,65 @@
 package cn.saintshaga.websocket.handler;
 
+import cn.saintshaga.websocket.WebsocketConnectionManager;
+import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class MyWebSocketHandler extends TextWebSocketHandler {
-	
+
+	private Map<String, WebSocketSession> allWebsockets;
+
+	public MyWebSocketHandler() {
+		allWebsockets = Maps.newConcurrentMap();
+	}
+
+	@Autowired
+	private WebsocketConnectionManager connectionManager;
+
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
-		System.out.println(message.getPayload());
+		String messageContent = message.getPayload();
+		System.out.println(messageContent);
+		if(!Strings.isNullOrEmpty(messageContent) && messageContent.contains("wait")) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Finish handling message:" + messageContent);
+		try {
+			session.sendMessage(new TextMessage("I have received:" + messageContent));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		allWebsockets.put(session.getId(),session);
+		connectionManager.addSession(session);
+		System.out.println("started:" + getSessionInfo(session));
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		allWebsockets.remove(session.getId());
+		connectionManager.removeSession(session);
+		System.out.println("stopped:" + getSessionInfo(session));
+	}
+
+	private String getSessionInfo(WebSocketSession session) {
+		return session.getId() + "," + session.getRemoteAddress();
+	}
+
+
+
 }
